21fa1b1e569e388c67b542f3e43cc4574ce956f7
Albedo <raincadence27@gmail.com>
2022-03-10 17:21:14 -0800

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,50 @@
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    messsage: 'A short description of your project',
+  },
+  {
+    type: 'input',
+    name: 'tableOfContents'
+    message: 'Section headers to list as a table of contents?'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Any special requirements/tips for installation?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Usage stipulations?',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'License information?',
+  },
+  {
+    type: 'input',
+    name: 'contributors',
+    message: 'Other contributors?',
+  },
+  {
+    type: 'input',
+    name: 'testing',
+    message: 'Test instructions?',
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'GitHub account to direct questions to',
+  },
 ];
 
 // function to write README file
